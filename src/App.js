275051bbec16ef085b30ef30539411e55ef8ec5b
Sanaual <sanaualhaque@gmail.com>
2020-04-28 18:01:09 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var Person = {
+    name : "Dr. Mahfuz",
+    job : 'Singer'
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit done <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My First React Paragraph</p>
+       
       </header>
     </div>
   );
